af2f0bf098184f4f42e813e85c88552b94a16be9
dejez30jr <165728707+dejez30jr@users.noreply.github.com>
2024-05-29 23:24:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const menuIcon = document.getElementById("menu-icon");
+const menulist = document.getElementById("menu-list");
+menuIcon.addEventListener("click", ()=>{
+    menulist.classList.toggle("hidden")
+})
+
+
+
+
+
+
+// alert('website ini hanya respons terhadap desktop untuk mobile seedang ada perbaikan');
+
+// var input_text = document.querySelector('#input_text')
+// var input_password = document.querySelector('#input_password')
+
+// $("submit").submit(function (e) {
+//   e.preventDefault
+
+//   if (input_text.value.length < 1)
+//   error_msg.style.display = "inline-block";
+//   input_text.style.border = "1px solid #f74040"
+//   return false;
+// })
